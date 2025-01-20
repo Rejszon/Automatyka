7e06991f04317d200d18363c5a94a614a30d41eb
Oskar Szymański <oskar.szymanski1608@gmail.com>
2025-01-20 23:23:20 +0100

    Finish project

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import plotly.express as px
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -7,35 +9,59 @@ def index():
     sliders = [
         {"name": "initalTemperature", "min": 1, "max": 99, "default": 25, "label": 'Temperatura początkowa wody'},
         {"name": "initialMass", "min": 1, "max": 10, "default": 4, "label": 'Masa początkowa wody'},
-        {"name": "setTemerature", "min": 10, "max": 99, "default": 20, "label": 'Ustalona temperatura podgrzewania wody'},
-        {"name": "simulationTime", "min": 60, "max": 1000, "default": 300, "label": 'Czas symulacji wyrażony w sekundach'},
+        {"name": "setTemerature", "min": 10, "max": 99, "default": 80, "label": 'Ustalona temperatura podgrzewania wody'},
+        {"name": "simulationTime", "min": 60, "max": 6000, "default": 1000, "label": 'Czas symulacji wyrażony w sekundach'},
         {"name": "heater", "min": 1000, "max": 3000, "default": 2000, "label": 'Moc grzałki'},
         {"name": "outdoorTemperature", "min": 0, "max": 35, "default": 22, "label": 'Temperatura otoczenia'},
-        {"name": "KP", "min": 0, "max": 600, "default": 300, "label": 'Człon proporcjonalny P'},
+        {"name": "KP", "min": 0, "max": 1000, "default": 300, "label": 'Człon proporcjonalny P'},
         {"name": "KI", "min": 0, "max": 1000, "default": 500, "label": 'Człon całkujący I'},
-        {"name": "KD", "min": 0, "max": 100, "default": 50, "label": 'Człon różniczkujący D'},
+        {"name": "KD", "min": 0, "max": 1000, "default": 50, "label": 'Człon różniczkujący D'},
+        {"name": "replenishMass", "min": 0, "max": 9, "default": 50, "label": 'Masa dolanej wody'},
+        {"name": "replenishTime", "min": 1, "max": 1000, "default": 50, "label": 'Czas dolania'},
+        {"name": "replenishTemperature", "min": 1, "max": 99, "default": 50, "label": 'Temperatura dolanej wody'},
     ]
 
     if request.method == 'POST':
         slider_values = request.form
-        processed_data = {key: value for key, value in slider_values.items()}
-        return render_template('result.html', data=processed_data)
+        processed_data = {key: int(value) for key, value in slider_values.items()}
+        T, Q = simulate(processed_data['simulationTime'], 
+        processed_data['initalTemperature'], 
+        processed_data['initialMass'], 
+        processed_data['setTemerature'], 
+        processed_data['heater'],
+        processed_data['outdoorTemperature'],
+        processed_data['KP'],
+        processed_data['KI'],
+        processed_data['KD'],
+        processed_data.get('replenish',0),
+        processed_data['replenishMass'],
+        processed_data['replenishTime'],
+        processed_data['replenishTemperature'],)
+        #Stwórz wykresy
+        time = list(range(processed_data['simulationTime'] + 1))
+        data = pd.DataFrame({
+        'Czas [s]': time,
+        'Temperatura [°C]': T,
+        'Moc grzałki [W]': Q
+        })
+
+        # Wykres temperatury od czasu
+        fig1 = px.line(data, x='Czas [s]', y='Temperatura [°C]', title='Zmiana temperatury w czasie')
+        fig1_temp = fig1.to_html(full_html=False)
+
+        # Wykres mocy grzałki od czasu
+        fig2 = px.line(data, x='Czas [s]', y='Moc grzałki [W]', title='Moc grzałki w czasie')
+        fig2_temp = fig2.to_html(full_html=False)
+
+        return render_template('result.html', plot1=fig1_temp, plot2=fig2_temp)
 
     return render_template('index.html', sliders=sliders)
-
-#To jest do dobrania obecnie losowe wartości z neta
-Kp = 1000
-Ki = 1
-Kd = 200
-
-maxQ = 2000 #Maksymalna moc grzałki
-outTemp = 25 #Temperatura na zewnątrz
 
 Cp = 4186 #ciepło właściwe płynu
 dt = 1 #krok czasowy pewnie zostawimy 1 na sztywno albo dodamy opcje wyboru
 Rt = 0.05 #Magic number który mówi o wymianie ciepła z otoczniem
 
-def simulate(simTime, initialTemp, currMass, setTemp):
+def simulate(simTime, initialTemp, currMass, setTemp, maxQ, outTemp, Kp, Ki, Kd ,repl, replMass, replTime, replTemp):
     temp = [initialTemp]
     q = [0]
     initialE = setTemp - initialTemp
@@ -54,6 +80,11 @@ def simulate(simTime, initialTemp, currMass, setTemp):
         #Obliczenie tempratury (T[k])
         C = currMass * Cp
         newTemp = currTemp + dt*((currQ / C) - (currTemp - outTemp)/(C * Rt)) 
+        
+        if repl and i == replTime:
+            currMass += replMass
+            newTemp = (currMass - replMass) / currMass * newTemp + (replMass / currMass) * replTemp
+            C = currMass * Cp
 
         temp.append(newTemp)
         q.append(currQ)
