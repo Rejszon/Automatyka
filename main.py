790abdc5ccedb3d4d9a8ee74ae7c22ddc7ba283a
Oskar Szymański <oskar.szymanski1608@gmail.com>
2025-01-22 12:21:51 +0100

    Minor adjustments

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, session
 from flask_session import Session
 import plotly.express as px
 import pandas as pd
-import math as ma
 
 app = Flask(__name__)
 app.secret_key = 'very_secret_key' #potrzebne do stworzenia sesji, w naszym przypadku bezpieczeństwo nie jest istotne
@@ -13,27 +12,25 @@ Session(app)
 def index():
     sliders = [
         {"name": "initalTemperature", "min": 1, "max": 99, "default": 25, "label": 'Temperatura początkowa wody'},
-        {"name": "initialMass", "min": 1, "max": 10, "default": 4, "label": 'Masa początkowa wody'},
-        {"name": "setTemerature", "min": 10, "max": 99, "default": 80, "label": 'Ustalona temperatura podgrzewania wody'},
-        {"name": "simulationTime", "min": 60, "max": 10000, "default": 5000, "label": 'Czas symulacji wyrażony w sekundach'},
-        {"name": "heater", "min": 1000, "max": 3000, "default": 2000, "label": 'Moc grzałki'},
+        {"name": "initialMass", "min": 1, "max": 7, "default": 4, "label": 'Objętość wody [l]'},
+        {"name": "setTemerature", "min": 10, "max": 85, "default": 70, "label": 'Ustalona temperatura podgrzewania wody'},
         {"name": "outdoorTemperature", "min": 0, "max": 35, "default": 22, "label": 'Temperatura otoczenia'},
-        {"name": "KP", "min": 0, "max": 1000, "step":2, "default": 100, "label": 'Człon proporcjonalny P'},
-        {"name": "KI", "min": 0, "max": 0.2, "step":0.001, "default": 0.04, "label": 'Człon całkujący I'},
-        {"name": "KD", "min": 0, "max": 50, "step":1, "default": 20, "label": 'Człon różniczkujący D'},
-        {"name": "replenishMass", "min": 0, "max": 9, "default": 2, "label": 'Masa dolanej wody'},
-        {"name": "replenishTime", "min": 1, "max": 9999, "default": 1000, "label": 'Czas dolania'},
-        {"name": "replenishTemperature", "min": 1, "max": 99, "default": 25, "label": 'Temperatura dolanej wody'},
+        {"name": "KP", "min": 0, "max": 1000, "step":2, "default": 100, "label": 'Wzmocnienie Regulatora'},
+        {"name": "KI", "min": 0, "max": 0.2, "step":0.001, "default": 0.04, "label": 'Czas Zdwojenia'},
+        {"name": "KD", "min": 0, "max": 50, "step":1, "default": 20, "label": 'Czas wyprzedzenia'},
+        {"name": "replenishMass", "min": 0, "max": 3, "default": 2, "label": 'Objętość dolewanej wody [l]'},
+        {"name": "replenishTime", "min": 1, "max": 5000, "default": 1000, "label": 'Moment rozpoczęcia dolewania wody [s]'},
+        {"name": "replenishTemperature", "min": 1, "max": 99, "default": 25, "label": 'Temperatura dolewanej wody'},
     ]
 
     if request.method == 'POST':
         slider_values = request.form
         processed_data = {key: float(value) for key, value in slider_values.items()}
-        T, Q = simulate(processed_data['simulationTime'], 
+        T, Q = simulate(5000, 
         processed_data['initalTemperature'], 
         processed_data['initialMass'], 
         processed_data['setTemerature'], 
-        processed_data['heater'],
+        2000,
         processed_data['outdoorTemperature'],
         processed_data['KP'],
         processed_data['KI'],
@@ -44,7 +41,7 @@ def index():
         processed_data['replenishTemperature'])
 
         #Stwórz wykresy
-        time = list(range(int(processed_data['simulationTime']) + 1))
+        time = list(range(int(5001)))
         data = pd.DataFrame({
         'Czas [s]': time,
         'Temperatura [°C]': T,
@@ -52,9 +49,9 @@ def index():
         })
 
         # Wykres temperatury od czasu
-        fig1 = px.line(data, x='Czas [s]', y='Temperatura [°C]', title='Zmiana temperatury w czasie')
+        fig1 = px.line(data, x='Czas [s]', y='Temperatura [°C]', title='Temperatura')
          # Wykres mocy grzałki od czasu
-        fig2 = px.line(data, x='Czas [s]', y='Moc grzałki [W]', title='Moc grzałki w czasie')
+        fig2 = px.line(data, x='Czas [s]', y='Moc grzałki [W]', title='Ciepło grzałki')
         
         hovertemplateT = "Czas [s]: %{x}<br>Temperatura [°C]: %{y}<extra></extra>"
         hovertemplateQ = "Czas [s]: %{x}<br>Moc grzałki [W]: %{y}<extra></extra>"
@@ -63,6 +60,8 @@ def index():
                 prev_df = pd.DataFrame(prev_data)
                 fig1.add_scatter(x=prev_df['Czas [s]'], y=prev_df['Temperatura [°C]'], mode='lines',name=f"Symulacja -{idx}", hovertemplate=hovertemplateT)
                 fig2.add_scatter(x=prev_df['Czas [s]'], y=prev_df['Moc grzałki [W]'], mode='lines',name=f"Symulacja -{idx}", hovertemplate=hovertemplateQ)
+        #Dodanie lini wartości zadanej
+        fig1.add_shape(type="line",x0=min(time), x1=max(time),y0=processed_data['setTemerature'], y1=processed_data['setTemerature'],line=dict(color="red", width=2, dash="dash"))
 
         fig1_temp = fig1.to_html(full_html=False)
         fig2_temp = fig2.to_html(full_html=False)
@@ -87,6 +86,7 @@ def simulate(simTime, initialTemp, currMass, setTemp, maxQ, outTemp, Kp, Ki, Kd 
     q = [0]
     initialE = setTemp - initialTemp
     integral = 0
+    addingWater = False
 
     for i in range(1, int(simTime)+1):
         #Obliczamy obecne e oraz temperature
@@ -103,12 +103,17 @@ def simulate(simTime, initialTemp, currMass, setTemp, maxQ, outTemp, Kp, Ki, Kd 
         newTemp = currTemp + dt*((currQ / C) - (currTemp - outTemp)/(C * Rt))
         
         if repl and i == replTime:
-            currMass += replMass
-            newTemp = (currMass - replMass) / currMass * newTemp + (replMass / currMass) * replTemp
+            addingWater = True
+        if addingWater:
+            replMass -= 0.20 #Przyjmujemy że dolanie litra zajmuje 5 sekund więc co sekunde zmieniamy mase o 0.20 
+            currMass += 0.20
+            newTemp = (currMass - 0.20) / currMass * newTemp + (0.20 / currMass) * replTemp
             C = currMass * Cp
-
-        temp.append(newTemp)
-        q.append(currQ)
+            if replMass <= 0:
+                addingWater = False
+        
+        temp.append(round(newTemp,2))
+        q.append(round(currQ))
         initialE = currE
     return temp,q
 
